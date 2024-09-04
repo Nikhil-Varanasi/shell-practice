dd07b6840d451e23644dbbc5c7b9d9174ef7e9e3
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-04 11:28:37 +0530

    adding coonversations script

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+!# /bin/bash
+
+echo "Ramesh:: Hi suresh, How are you?"
+echo "suresh:: hi ramesh, i am fine, how are you?"
+echo "Ramesh:: I am doing good suresh, whats going on?"
+echo "suresh:: I started learing shell scripting"
