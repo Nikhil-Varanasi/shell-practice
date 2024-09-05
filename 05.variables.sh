ed0453ac7173a50a7eebb8da42060208e13c6da7
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-05 16:24:07 +0530

    reading inputs during runtiime

---
+++ 05.variables.sh
@@ -0,0 +1,7 @@
+!# /bin/bash
+echo "Please enter your username"
+read username
+echo "username entered is:: $username"
+echo "please enter your passowrd"
+read -s password
+echo "password entered is:: $password"
