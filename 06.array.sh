e98c613ac307763bc2129a249ad1fa72d7c1b02e
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-05 16:43:01 +0530

    arrays and index concepts

---
+++ 06.array.sh
@@ -0,0 +1,12 @@
+!# /bin/bash
+
+#index starts from 0 , soze start from 1
+
+Fruits=("Apple","kiwi","orange") #Array
+
+echo "First frut is:${Fruits[0]}"
+echo "First fruit is:${Fruits[1]}"
+echo "First fruit is:${Fruits[2]}"
+
+echo "First fruit is:${Fruits[@]}"
+
