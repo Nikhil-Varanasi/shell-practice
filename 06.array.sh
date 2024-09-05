883cfe0368e16cd6e20761583cf23a75ea2c5861
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-05 16:50:06 +0530

    small systex changes to array file

--- 06.array.sh
+++ 06.array.sh
@@ -2,7 +2,7 @@
 
 #index starts from 0 , soze start from 1
 
-Fruits=("Apple","kiwi","orange") #Array
+Fruits=("Apple" "kiwi" "orange") #Array
 
 echo "First frut is:${Fruits[0]}"
 echo "First fruit is:${Fruits[1]}"
