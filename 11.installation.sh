50668c6243da30aacf7c98640a15d2ea123f3646
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-06 12:45:46 +0530

    adding lines

--- 11.installation.sh
+++ 11.installation.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 USERID=$(id -u)               #id -u will id number alone
-echo "user id is : $USERID"    
+# echo "user id is : $USERID"    
 
 if [ $USERID -ne 0 ]
 then
     echo "please run this scritp with root privelages"
+    exit 1
 fi
 #dnf install git -y
