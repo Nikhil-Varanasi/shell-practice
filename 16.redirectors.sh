a454cf9b5eff3acf887f845e51977fed1e6cbe44
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-07 20:14:28 +0530

    some new lines of code

--- 16.redirectors.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-LOGS_FOLDER= "/var/log/shell-script"
-SCRIPT_NAME= "$(echo $0 | cut -d "." -f1)"
-TIMESTAMP= $(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE= "$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-
-echo $LOG_FILE
