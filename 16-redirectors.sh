84f4a730f3e4a953eae82ed03df53f3de40d9639
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-07 20:17:01 +0530

    some new lines of code

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-# LOGS_FOLDER= "/var/log/shell-script"
-# SCRIPT_NAME= "$(echo $0 | cut -d "." -f1)"
-# TIMESTAMP= $(date +%Y-%m-%d-%H-%M-%S)
-# LOG_FILE= "$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
-# echo $LOG_FILE
+mkdir -p $LOGS_FOLDER
+# echo $LOGS_FOLDER
 
 # /var/log/shell-script/echo 16-redirectories.sh | cut -d "." -f1 
 
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+# LOGS_FOLDER="/var/log/shell-script"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+# mkdir -p $LOGS_FOLDER
