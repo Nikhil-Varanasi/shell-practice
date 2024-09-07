b671257875f2937f52796b99340845c80cf71886
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-07 20:17:53 +0530

    some new lines of code

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -4,14 +4,4 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-
 mkdir -p $LOGS_FOLDER
-# echo $LOGS_FOLDER
-
-# /var/log/shell-script/echo 16-redirectories.sh | cut -d "." -f1 
-
-# LOGS_FOLDER="/var/log/shell-script"
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-# mkdir -p $LOGS_FOLDER
