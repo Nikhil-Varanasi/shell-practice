145aeb3b61c5f842806caa24a6c09ee7ff58c64b
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-08 22:27:34 +0530

    adding backend and frontend files

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -ex #setting the automatic exit, if we get error, x for debug
+#set -ex #setting the automatic exit, if we get error, x for debug
 failure(){
     echo "Failed at: $1:$2"
 }
