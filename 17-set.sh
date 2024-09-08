87173fd425b226e58c0eb9e132f82fddb3c68e90
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-08 22:22:12 +0530

    adding backend and frontend files

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 set -ex #setting the automatic exit, if we get error, x for debug
+failure(){
+    echo "Failed at: $1:$2"
+}
+trap 'failure "${LINENO}" "$BASH_COMMAND" ' ERR #ERR is the error signal
 echo "hello world...succsess"
 echooo " hello world .. failure"
 echo "hello world after failure.."
