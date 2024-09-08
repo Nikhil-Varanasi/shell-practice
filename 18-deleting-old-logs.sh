1248821c3cc7ac8dff9fad3c9f32da20e5a7ea6c
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-08 22:59:52 +0530

    adding backend and frontend files

--- 18-deleting-old-logs.sh
+++ 18-deleting-old-logs.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
 SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ -d $SOURCE_DIR ]
 then
-    echo "$SOURCE_DIR exist"
+    echo -e "$SOURCE_DIR $G exist $N"
 else
-    echo "$SOURCE_DIR does not exist"
+    echo -e "$SOURCE_DIR $G does not exist $N"
     exit 1
 fi
 
