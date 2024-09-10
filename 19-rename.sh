a7cc51dc18ed4bc96e2120d04bc197c97e47496e
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-10 08:41:09 +0530

    some new lines of code

---
+++ 19-rename.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #if they don't mention 3rd variavle it will tale 14 as default
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USAGGE(){
+    echo -e "$R USAGE$N:: sh 19-rename.sh <source> <destination> <days(optional)>"
+}
+
+#check if the source and destinon is provided or not
+if [ $# -lt 2]
+then
+    USAGE
+fi  
