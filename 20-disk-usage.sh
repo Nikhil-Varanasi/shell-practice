350ec072a17bf0c99f5a608e05ff72d4f9b2e41f
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-09-13 13:58:25 +0530

    addning lines

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -3,6 +3,10 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real project , itis usally 75
 while IFS= read -r line
 do
-    echo $line
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD,Current value is $USAGE,please check"
+    fi    
 done <<< $DISK_USAGE
-    
